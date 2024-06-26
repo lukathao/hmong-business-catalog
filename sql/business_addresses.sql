88b746dcbf29b9a976e622be65175baa7ffc728b
lukathao <lukayeethao@gmail.com>
2024-06-25 19:57:33 -0500

    initial commit

---
+++ sql/business_addresses.sql
@@ -0,0 +1,8 @@
+create table business_address (
+  business_id varchar(32),
+  address varchar(100),
+  address_city varchar(100),
+  address_state varchar(100),
+  address_code varchar(32),
+  coordinates varchar(32)
+)
---
+++ sql/businesses.sql
@@ -0,0 +1,9 @@
+create table businesses (
+  id uuid primary key,
+  business_name varchar(100),
+  business_owner varchar(100),
+  is_active boolean,
+  business_id varchar(100),
+  created_at timestamp,
+  updated_at timestamp
+)
