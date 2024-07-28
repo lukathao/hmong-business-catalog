f542ac5bc49ea10c6a3beb92bd9131f67debe322
lukathao <lukayeethao@gmail.com>
2024-07-28 16:03:05 -0500

    removed neondbservice

--- src/repository/neonDbRepo.ts
+++ src/repository/neonDbRepo.ts
@@ -1,8 +1,10 @@
-import { getNeonDBPool } from '@/utils/services/NeonDbService';
+import { Pool } from '@neondatabase/serverless';
 import sqlstring from "sqlstring";
 
 export const getAllBusinessesFromNeonDb = async () => {
-    const pool = await getNeonDBPool();
+    const pool = await new Pool({
+      connectionString: process.env.NEON_DATABASE_URL,
+    });
     const sql = sqlstring.format(`
       select business_owner, business_name, id from businesses  
     `, []);
